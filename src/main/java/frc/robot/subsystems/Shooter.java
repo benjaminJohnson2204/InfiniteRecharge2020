7f1c91bac08790f15ba1bd20816a219583853fec
Kai Lyou <kai_lyou@student.davincischools.org>
2020-01-10 14:25:20 -0800

    Shooter Pseudocode

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -13,9 +13,33 @@ public class Shooter extends SubsystemBase {
   /**
    * Creates a new ExampleSubsystem.
    */
-  public Shooter() {
 
+  private TalonSRX[] outtakeMotors = {
+          new TalonSRX(40); //subject to change, 40 is placeholder
+          //initialize motors here
   }
+  public Shooter() {
+    super("Shooter"); //do not know what this does
+
+    for(TalonSRX outtakeMotors:outtakeMotor){
+      outtakeMotor.configFactoryDefault(); //configure the motors
+      outtakeMotor.setNeutralMode(NeutralMode.Coast)
+    }
+    //set any motors inverted/follow if necessary
+  }
+
+  public void startSpin(double output){outtakeMotors[0].set(ControlMode.PercentOutput, output);}
+
+  public void insertCell(){/*dont know how to do this mechanically*/}
+
+  public void angleHood(double distance){
+    //calculate angle necessary to shoot
+    //determine difference in angles
+    //use either pid loop or simpler loop to correct error
+  }
+
+
+
 
   @Override
   public void periodic() {
