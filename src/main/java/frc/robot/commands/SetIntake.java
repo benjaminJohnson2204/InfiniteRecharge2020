62150315898f7f32675ebe236f0ca7f66a012ca5
Jet <jet_pham@student.davincischools.rg>
2020-01-10 19:46:50 -0800

    intake commands added
    its explained in the title. that's it

--- src/main/java/frc/robot/commands/SetIntake.java
+++ src/main/java/frc/robot/commands/SetIntake.java
@@ -32,18 +32,19 @@ public class SetIntake extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    intake.setOutput(true);
+    intake.intake();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    intake.setOutput(false);
+    intake.stop();
   }
 
   // Returns true when the command should end.
