c474f12a44543c721745e05a8b9c6dcad21adcdd
Unknown <jonathandao0@gmail.com>
2020-01-12 14:37:57 -0800

    Changes
    -Added csv reader for trajectories
    -Added BadLog implementation from 2019

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -33,6 +34,10 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+    System.out.println("Test");
+    String filePath = Filesystem.getDeployDirectory().getAbsolutePath() + "/Trajectories/";
+    System.out.println(filePath);
   }
 
   /**
