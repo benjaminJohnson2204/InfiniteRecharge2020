4d46a81021b963b4a46586f3aefbfc48aaafb816
Jonathan Dao <jonathandao0@gmail.com>
2020-02-15 14:03:02 -0800

    Changes
    -Added isFinished() conditions to ReadTrajectory()

--- src/main/java/frc/robot/commands/autonomous/ReadTrajectory.java
+++ src/main/java/frc/robot/commands/autonomous/ReadTrajectory.java
@@ -37,9 +37,9 @@ public class ReadTrajectory extends CommandBase {
   private String m_filename;
   private boolean m_isInverted = false;
   private Timer testTimer = new Timer();
-  private double firstTime;
+  private double timeout, distance;
   private DifferentialDriveKinematicsConstraint m_kinematicsConstraint;
-  RamseteCommand followTrajectory;
+  ArrayList<Pose2d> trajectoryWaypoints = new ArrayList<Pose2d>();
   /**
    * Creates a new ExampleCommand.
    *
@@ -74,7 +74,7 @@ public class ReadTrajectory extends CommandBase {
     // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    firstTime = testTimer.getFPGATimestamp();
+    trajectoryWaypoints = new ArrayList<Pose2d>();
 //    m_driveTrain.navX.reset();
     m_driveTrain.setDriveTrainNeutral();
 //    m_driveTrain.resetOdometry(new Pose2d(), new Rotation2d());
@@ -87,7 +87,6 @@ public class ReadTrajectory extends CommandBase {
     String filePath = Filesystem.getDeployDirectory().getAbsolutePath() + "/Trajectories/" + m_filename + ".csv";
     var fileTrajectory = ReadCsvTrajectory.readCsv(filePath);
 
-    var trajectoryWaypoints = new ArrayList<Pose2d>();
     trajectoryWaypoints.add(startPosition);
 
     // All points we generate assume we start from (0,0). Take those points and shift it based on your starting position
@@ -102,6 +101,18 @@ public class ReadTrajectory extends CommandBase {
 
     var trajectoryConfig = new TrajectoryConfig(Units.feetToMeters(16),  Units.feetToMeters(8));
 
+    distance = 0;
+    for(int i = 0; i < trajectoryWaypoints.size() - 1; i++) {
+      var pointA = trajectoryWaypoints.get(i);
+      var pointB = trajectoryWaypoints.get(i + 1);
+
+      double deltaX = pointB.getTranslation().getX() - pointA.getTranslation().getX();
+      double deltaY = pointB.getTranslation().getY() - pointA.getTranslation().getY();
+      double deltaDistance = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+      distance += deltaDistance;
+    }
+    timeout = (distance / 16.0) + 2;
+
     if(m_kinematicsConstraint != null)
       trajectoryConfig.addConstraint(m_kinematicsConstraint);
 
@@ -109,7 +120,7 @@ public class ReadTrajectory extends CommandBase {
 
     trajectory = TrajectoryGenerator.generateTrajectory(trajectoryWaypoints, trajectoryConfig);
 
-    followTrajectory = new RamseteCommand(
+    RamseteCommand followTrajectory = new RamseteCommand(
             trajectory,
             m_driveTrain::getRobotPose,
             new RamseteController(),
@@ -122,6 +133,7 @@ public class ReadTrajectory extends CommandBase {
             m_driveTrain
     );
     CommandScheduler.getInstance().schedule(followTrajectory);
+    testTimer.start();
   }
 
   // Called once the command ends or is interrupted.
@@ -132,7 +144,10 @@ public class ReadTrajectory extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return followTrajectory.isFinished();
+    double deltaX = Math.abs(m_driveTrain.getRobotPose().getTranslation().getX() - trajectoryWaypoints.get(trajectoryWaypoints.size()).getTranslation().getX());
+    double deltaY = Math.abs(m_driveTrain.getRobotPose().getTranslation().getY() - trajectoryWaypoints.get(trajectoryWaypoints.size()).getTranslation().getY());
+    double deltaRot = Math.abs(m_driveTrain.getRobotPose().getRotation().getDegrees() - trajectoryWaypoints.get(trajectoryWaypoints.size()).getRotation().getDegrees());
+    return ((deltaX < 0.25) && (deltaY < 0.25) && (deltaRot < 2)) || testTimer.get() > timeout;
   }
 
 }
