0b91961fc97ada60571caabc177a4e51f92cdd02
DerrickB3 <44348986+DerrickB3@users.noreply.github.com>
2020-02-15 17:00:30 -0800

    Changes
    -Align drivetrain to ball
    -Vision subsystem improvements

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -22,7 +22,6 @@ import frc.robot.commands.autonomous.TestPathFollowing;
 import frc.robot.commands.drivetrain.SetArcadeDrive;
 import frc.robot.commands.drivetrain.ZeroDriveTrainEncoders;
 import frc.robot.commands.skyhook.SetSkyhook;
-import frc.robot.commands.vision.AlignToOuterPort;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -77,8 +76,6 @@ public class RobotContainer {
     CommandScheduler.getInstance().schedule(new ZeroDriveTrainEncoders(m_driveTrain));
 
     m_skyhook.setDefaultCommand(new SetSkyhook(m_skyhook));
-
-    m_vision.openSightInit();
   }
 
   /**
@@ -99,9 +96,6 @@ public class RobotContainer {
 
     xBoxLeftTrigger = new XBoxTrigger(xBoxController, 2);
     xBoxRightTrigger = new XBoxTrigger(xBoxController, 3);
-
-    // Limelight vision alignment
-    rightButtons[0].whenPressed(new AlignToOuterPort(m_driveTrain, m_vision));
   }
 
   public static double getLeftJoystickX() {
--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -17,73 +17,61 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
-public class Vision extends SubsystemBase
-{
+public class Vision extends SubsystemBase {
     private NetworkTable limelight;
-    private UsbCamera driverCam;
+    private NetworkTable openSight;
 
     // Variables for calculating distance
     private final double TARGET_HEIGHT = 98.25; // Outer port height above carpet in inches
-    private final double LIMELIGHT_MOUNT_ANGLE = 26.5; // Angle that the Limelight is mounted at
+    private final double LIMELIGHT_MOUNT_ANGLE = 63.5; // Angle that the Limelight is mounted at
     private final double LIMELIGHT_HEIGHT = 37.31; // Limelight height above the ground in inches
 
-    public Vision()
-    {
+    public Vision() {
         limelight = NetworkTableInstance.getDefault().getTable("limelight");
+        openSight = NetworkTableInstance.getDefault().getTable("OpenSight");
     }
 
-    public double getTargetY()
-    {
+    public double getTargetY() {
         return limelight.getEntry("ty").getDouble(0);
     }
 
-    public double getTargetX()
-    {
+    public double getTargetX() {
         return limelight.getEntry("tx").getDouble(0);
     }
 
-    public boolean hasTarget()
-    {
+    public boolean hasTarget() {
         return limelight.getEntry("tv").getDouble(0) == 1;
     }
 
-    public double getTargetArea()
-    {
+    public double getTargetArea() {
         return limelight.getEntry("ta").getDouble(0);
     }
 
-    public double getTargetSkew()
-    {
+    public double getTargetSkew() {
         return limelight.getEntry("ts").getDouble(0);
     }
 
-    public double getPipelineLatency()
-    {
+    public double getPipelineLatency() {
         return limelight.getEntry("tl").getDouble(0);
     }
 
-    public double getTargetShort()
-    {
+    public double getTargetShort() {
         return limelight.getEntry("tshort").getDouble(0);
     }
 
-    public double getTargetLong()
-    {
+    public double getTargetLong() {
         return limelight.getEntry("tlong").getDouble(0);
     }
 
-    public double getHorizontalSidelength()
-    {
+    public double getHorizontalSidelength() {
         return limelight.getEntry("thor").getDouble(0);
     }
 
-    public double getVerticalSidelength()
-    {
+    public double getVerticalSidelength() {
         return limelight.getEntry("tvert").getDouble(0);
     }
 
-    public double getPipeline()
-    {
+    public double getPipeline() {
         return limelight.getEntry("getpipe").getDouble(0);
     }
 
@@ -102,45 +90,90 @@ public class Vision extends SubsystemBase
         limelight.getEntry("pipeline").setNumber(pipeline);
     }
 
-    public double getTargetDistance()
-    {
-        double angleToTarget = getTargetY();
-        return (TARGET_HEIGHT - LIMELIGHT_HEIGHT) / Math.tan(LIMELIGHT_MOUNT_ANGLE + angleToTarget);
+    public double getTargetDistance() {
+        double[] distances = new double[5];
+        for (int i = 0; i < distances.length; i++) {
+            double angleToTarget = getTargetY();
+            double inches = (TARGET_HEIGHT - LIMELIGHT_HEIGHT) / Math.tan(LIMELIGHT_MOUNT_ANGLE + angleToTarget);
+            distances[i] = inches / 12;
+        }
+
+        double distance = computeMode(distances);
+
+        // Adjust distance based on pipeline (zoom level)
+        int pipeline = (int)getPipeline();
+        switch (pipeline) {
+            case 1:
+                // TODO
+                break;
+
+            case 2:
+                // TODO
+                break;
+
+            case 3:
+                // TODO
+                break;
+        }
+
+        return distance;
+    }
+
+    private double computeMode(double[] data) {
+        // Compute mode
+        double[] counts = new double[data.length]; // Does this need to be zero initialized?
+        for (int i = 0; i < data.length; i++)
+        {
+            for (int j = 0; j < data.length; i++)
+            {
+                if (data[i] == data[j])
+                {
+                    counts[i]++;
+                }
+            }
+        }
+
+        int highestIndex = 0;
+        double previousHigh = 0;
+        for (int i = 0; i < counts.length; i++)
+        {
+            if (counts[i] > previousHigh)
+            {
+                highestIndex = i;
+                previousHigh = counts[i];
+            }
+        }
+
+        return data[highestIndex]; // Final distance in feet
+    }
+
+    public double getPowerCellX() {
+        double xOffset = openSight.getEntry("ball").getDoubleArray(new double[] {0, 0})[0];
+
+        // TODO: Use xOffset to return a positive value when target is the right and vice versa for right
+
+        return xOffset;
+    }
+
+    public boolean hasPowerCell() {
+        return openSight.getEntry("found").getBoolean(false);
     }
 
     /*
-    public void initUSBCamera()
-    {
-        try
-        {
-            driverCam = CameraServer.getInstance().startAutomaticCapture();
-            driverCam.setResolution(160, 160);
-            driverCam.setConnectionStrategy(VideoSource.ConnectionStrategy.kKeepOpen);
-            driverCam.setFPS(15);
-            driverCam.setExposureManual(50);
-        }
-        catch (Exception e)
-        {
-
-        }
-    }
-     */
-
-    public void openSightInit()
-    {
+    public void openSightInit() {
         // Seems to be necessary to get OpenSight cam to show up in Shuffleboard
         // CameraServer.getInstance().addAxisCamera("opensight", "opensight.local");
         CameraServer.getInstance().addServer("opensight.local");
     }
+     */
 
-    public void updateSmartDashboard()
-    {
+    public void updateSmartDashboard() {
         SmartDashboard.putNumber("Limelight Target Distance", getTargetDistance());
+        SmartDashboard.putNumber("Limelight Pipeline", getPipeline());
     }
 
     @Override
-    public void periodic()
-    {
+    public void periodic() {
         // This method will be called once per scheduler run
         updateSmartDashboard();
     }
