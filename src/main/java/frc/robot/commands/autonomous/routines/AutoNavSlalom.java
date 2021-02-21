cc5b287a4cbb37e552386e6f85af5fcf9a3a9203
Unknown <jonathandao0@gmail.com>
2021-02-21 14:53:10 -0800

    Add Images

--- src/main/java/frc/robot/commands/autonomous/routines/AutoNavSlalom.java
+++ src/main/java/frc/robot/commands/autonomous/routines/AutoNavSlalom.java
@@ -1,39 +1,22 @@
 package frc.robot.commands.autonomous.routines;
 
-import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.geometry.Transform2d;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboardTab;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj.trajectory.constraint.CentripetalAccelerationConstraint;
 import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveKinematicsConstraint;
 import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.wpilibj.trajectory.constraint.EllipticalRegionConstraint;
 import edu.wpi.first.wpilibj.util.Units;
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.RobotContainer;
-import frc.robot.commands.autonomous.SmartdashboardCommand;
-import frc.robot.commands.autonomous.TurnInPlace;
 import frc.robot.commands.drivetrain.SetDriveNeutralMode;
-import frc.robot.commands.drivetrain.SetDriveShifters;
 import frc.robot.commands.drivetrain.SetOdometry;
-import frc.robot.commands.intake.SetIntakeSpeed;
-import frc.robot.commands.intake.SetIntakePiston;
-import frc.robot.commands.intake.SetIntakeStates;
 import frc.robot.simulation.FieldSim;
-import frc.robot.simulation.SimulationShoot;
-import frc.robot.subsystems.*;
+import frc.robot.simulation.SimConstants;
+import frc.robot.subsystems.DriveTrain;
 import frc.vitruvianlib.utils.TrajectoryUtils;
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class AutoNavSlalom extends SequentialCommandGroup {
@@ -69,7 +52,19 @@ public class AutoNavSlalom extends SequentialCommandGroup {
         //configA.setEndVelocity(configA.getMaxVelocity());
         configA.addConstraint(new DifferentialDriveKinematicsConstraint(driveTrain.getDriveTrainKinematics(), configA.getMaxVelocity()));
         configA.addConstraint(new DifferentialDriveVoltageConstraint(driveTrain.getFeedforward(), driveTrain.getDriveTrainKinematics(),10));
-        configA.addConstraint(new CentripetalAccelerationConstraint(0.45));
+//        configA.addConstraint(new CentripetalAccelerationConstraint(0.45));
+
+        int numberOfCones = SimConstants.autoNavSlalomCones.length;
+            for(int i = 0; i < numberOfCones; i++) {
+            var constraint = new EllipticalRegionConstraint(
+                    SimConstants.autoNavSlalomCones[i],
+                    Units.inchesToMeters(20),
+                    Units.inchesToMeters(20),
+                    new Rotation2d(),
+                    new DifferentialDriveKinematicsConstraint(driveTrain.getDriveTrainKinematics(), 0)
+            );
+                //configA.addConstraint(constraint);
+        }
 
         Trajectory tempTrajectory;
         Pose2d finalPose = waypoints[0];
@@ -98,6 +93,8 @@ public class AutoNavSlalom extends SequentialCommandGroup {
                 List.of(),
                 waypoints[i + 1],
                 configA);
+
+
             var command = TrajectoryUtils.generateRamseteCommand(driveTrain, trajectory);
             addCommands(command);//.andThen(() -> SmartDashboard.putNumber("Final Pose angle", trajectory.)));
             /*SmartDashboard.putNumber("Initial Pose x", tempTrajectory.getInitialPose().getX());
